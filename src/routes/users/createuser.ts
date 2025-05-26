913761e03de9eefe826646dc26913db331735079
Gopal Sarkar <gopalsarkar.dev@gmail.com>
2025-05-26 12:44:46 +0530

    usercreate-route✔️ and migrate

--- src/routes/users/createuser.ts
+++ src/routes/users/createuser.ts
@@ -20,9 +20,35 @@ const createuser: FastifyPluginAsyncTypebox = async (
     handler: async (request, reply) => {
       const { email, first_name, gender, last_name } = request.body;
 
+      const userDetail = {
+        first_name: first_name.trim(),
+        last_name: last_name.trim(),
+        email: email.toLocaleLowerCase(),
+        gender: gender,
+      };
+
       try {
+        const alreadyUser = await fastify.prisma.user.findUnique({
+          where: {
+            email: email,
+          },
+        });
+
+        if (alreadyUser !== null) {
+          return reply.conflict(`${email} already register`);
+        }
+
+        await fastify.prisma.user.create({
+          data: userDetail,
+        });
+
+        return reply.code(201).send({
+          message: "User Successfuly created",
+        });
       } catch (error) {
         console.log(error);
+
+        return reply.code(500).internalServerError("User create failed");
       }
     },
   });
